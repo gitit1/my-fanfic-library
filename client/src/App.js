4bc909e26a80ad3fbd418fd31d345f96e841732b
Gitit <gitit1@msn.com>
2019-05-31 11:10:25 +0300

    add routing, fandom and fandoms containers

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 
 import './App.css';
 import Layout from './hoc/Layout/Layout';
@@ -8,6 +9,13 @@ function App() {
   return (
     <Layout>
       <Index/>
+      <Switch>
+        {/* <Route path="/fandoms" component={asyncAuth} /> */}
+        <Route path="/fandom" exact component={BurgerBuilder} />
+        {/* <Route path="/search" exact component={BurgerBuilder} /> */}
+        {/* <Route path="/my_tracking" exact component={BurgerBuilder} /> */}
+        <Redirect to="/" />
+      </Switch>
     </Layout>
   );
 }
