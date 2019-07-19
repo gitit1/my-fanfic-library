ca5b4c458085055699aa27b0b31d23f72952fc93
Gitit <gitit1@msn.com>
2019-07-20 00:48:22 +0300

    adjestments to vps

--- client/src/store/axios-server.js
+++ client/src/store/axios-server.js
@@ -4,7 +4,7 @@ console.log('process.env.NODE_ENV:',process.env.NODE_ENV)
 
 const instance = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') 
                  ? axios.create({baseURL: 'http://localhost:5000'})
-                 : axios.create({baseURL: 'https://192.236.176.82:5000'});
+                 : axios.create({baseURL: '192.236.176.82:5000'});
 
 
 export default instance;
