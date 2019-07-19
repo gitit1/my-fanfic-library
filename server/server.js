49e53c2b5e54e1fc159f6bdc796a858914bd3bc8
Gitit <gitit1@msn.com>
2019-07-19 23:54:02 +0300

    adjestments to vps

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
@@ -39,45 +38,4 @@ app.use('/',routes);
 
 
 
-=======
-const express = require('express');
-const bodyParser = require('body-parser');
-const routes = require('./routes');
-const cors = require('cors');
-const passport = require("passport");
-
-const publicDir = require('path').join(__dirname,'/public');
-const users = require("./controllers/users");
-// require('dotenv').config({
-//     path: 'variables.env'
-// });
-
-require('./config/mongoose.js')
-require('./controllers/socket/socket');
-//TODO: check the cronjob scuduale
-require('./cronJobs/cron')
-
-const app = express();
-
-// Passport middleware
-app.use(passport.initialize());
-// Passport config
-require("./config/passport")(passport);
-
-
-// const port = process.env.PORT || 5000;
-const port = 5000;
-
-
-
-app.use(cors())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(publicDir));
-
-app.use('/',routes);
-
-
-
->>>>>>> Stashed changes
 app.listen(port, () => console.log(`Listening on port ${port}`));
