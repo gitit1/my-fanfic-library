4b10b485183688c7a8278928a1ee1218c041c217
Gitit <gitit1@msn.com>
2019-07-24 22:12:54 +0300

    client - turning header and footer into scss , server - deleteing socket from ao3.js

--- client/src/hoc/Layout/Footer/Footer.js
+++ client/src/hoc/Layout/Footer/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import classes from './Footer.module.css';
+import './Footer.scss'
 
 const Footer = () => (
-  <div className={classes.Footer}>
+  <div className={'footer_section header'}>
       <br/>
       <p><b>Desclaimer:</b> ALL the RIght for the fanfics and information belongs to <a href="https://archiveofourown.org/">AO3</a>.</p>
       <p>This site was build only for the purpose of saving my favorites fanfics and so it will be easier to know what I already read</p>
