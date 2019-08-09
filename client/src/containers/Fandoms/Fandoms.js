c300dfdb9f2e594c202a66b0343332b9d027535f
Gitit <gitit1@msn.com>
2019-08-09 17:06:06 +0300

    all fandoms - changes design

--- client/src/containers/Fandoms/Fandoms.js
+++ client/src/containers/Fandoms/Fandoms.js
@@ -8,9 +8,22 @@ import Container from '../../components/UI/Container/Container';
 
 import ShowFandomData from './ShowFandomData/ShowFandomData';
 
+
 class AllFandoms extends Component{
+    state ={
+      screenSize:null,
+      mobileSize: 736,
+      medSize:1000
+    }
+
+    componentWillMount(){this.setState({screenSize:window.innerWidth})}
+
     render(){
-        let page =  null
+        const {screenSize,mobileSize,medSize} = this.state;
+        let size = (screenSize>medSize) ? 'l' : (screenSize>mobileSize) ? 'm' : 's';
+        console.log('screenSize:',screenSize)
+        console.log('size:',size)
+        let page =  null;
         if(!this.props.loading){
           if(this.props.fandoms.length === 0||this.props.fandoms ===null){
               page = (
@@ -20,7 +33,7 @@ class AllFandoms extends Component{
               )
           }else{
               const sortedFandomList = this.props.fandoms.sort((a, b) => a.FandomName.localeCompare(b.FandomName))
-              page = (<ShowFandomData fandoms={sortedFandomList}/>)
+              page = (<ShowFandomData fandoms={sortedFandomList} screenSize={size}/>)
                       
           }
         }
--- client/src/containers/Fandoms/ShowFandomData/ShowFandomData.js
+++ client/src/containers/Fandoms/ShowFandomData/ShowFandomData.js
@@ -1,13 +1,91 @@
 import React from 'react';
 import classes from './ShowFandomData.module.css';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import ListSubheader from '@material-ui/core/ListSubheader';
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        
+        overflow: 'hidden',
+        margin: '0 auto'
+    },
+    gridList: {
+        width: '100%',
+        height: 'auto',
+        justifyContent: 'center',
+    },
+    card: {
+      maxWidth: 400,
+    },
+    media: {
+      height: 250,
+    }
+  });
 
 const ShowFandomData = (props) => {
+    const {screenSize,fandoms} = props,classesMterial = useStyles();
+
+    const cellHeight = (screenSize==='l'||screenSize==='m') ? 400 : 240;
+    const cols = (screenSize==='l') ? 3 : (screenSize==='m') ? 2 : 1;
+    let length = fandoms.length;
     return(
         <React.Fragment>
-                <div className={classes.Fandoms}>
-                  { props.fandoms.map(fandom=>(
+            <div className={classesMterial.root} style={(screenSize==='l'||screenSize==='m') ? {width:'90%'} : {width:'100%'}}>
+                <GridList cellHeight={cellHeight} className={classesMterial.gridList} cols={cols}>
+                    {fandoms.map(fandom=>(
+                        // <GridListTile cols={1}>
+                            <Card className={`${classesMterial.card} ${classes.Fandom}`} key={fandom.FandomName} >
+                                <CardActionArea>
+                                    <Link to={`/fanfics/${fandom.FandomName}`}>
+                                        <CardMedia  className={classesMterial.media} 
+                                                    image={fandom.Image_Name !== '' 
+                                                            ? `/fandoms/${fandom.FandomName.toLowerCase()}/${fandom.Image_Name}`
+                                                            : `/fandoms/nophoto.png`
+                                                    } 
+                                                    title={fandom.FandomName}/>
+                                        <CardContent className={classes.mobileMode}>
+                                            {(screenSize==='l'||screenSize==='m')
+                                             ?                                                  
+                                                <React.Fragment>
+                                                    <Typography gutterBottom variant="h5" component="h2">{fandom.FandomName}</Typography>
+                                                    <Typography variant="body2" color="textSecondary" component="p">
+                                                        <span>Fanfics in Fandoms: </span><b>{fandom.FanficsInFandom.toLocaleString(undefined, {maximumFractionDigits:2})}</b>
+                                                    </Typography>       
+                                                    <Typography variant="body2" color="textSecondary" component="p">
+                                                    <span>Complete Fanfics: </span>{fandom.CompleteFanfics.toLocaleString(undefined, {maximumFractionDigits:2})}
+                                                    </Typography>       
+                                                    <Typography variant="body2" color="textSecondary" component="p">
+                                                    <span>In Progress Fanfics: </span>{fandom.OnGoingFanfics.toLocaleString(undefined, {maximumFractionDigits:2})} 
+                                                    </Typography>       
+                                                </React.Fragment>
+                                             :  <div  className={classes.overlay}>
+                                                    <Typography className={classes.overlayP} gutterBottom variant="h5" component="h2">{fandom.FandomName}</Typography>
+                                                </div>
+                                            }
+                                        </CardContent>
+                                    </Link>
+                                </CardActionArea>
+                            </Card>
+                        // </GridListTile>
+                    ))} 
+                    {( (screenSize==='l'||screenSize==='m') && (length>cols && (length%cols!==0) && ((length-(Math.floor(length/cols)*cols)<=1)) ) ) && <Card className={`${classesMterial.card} ${classes.Fandom} ${classes.Dummy}`} />}
+                    {( (screenSize==='l'||screenSize==='m') && (length>cols && (length%cols!==0) && ((length-(Math.floor(length/cols)*cols)<=2)) ) ) && <Card className={`${classesMterial.card} ${classes.Fandom} ${classes.Dummy}`} />}
+
+                </GridList>
+            </div>
+                  {/* { props.fandoms.map(fandom=>(
                       <div className={classes.Fandom} key={fandom.FandomName}>
                           <section className={classes.ImageSection}>
                               {
@@ -34,7 +112,7 @@ const ShowFandomData = (props) => {
                   ))}
                   <div className={[classes.Dummy,classes.Fandom].join(' ')}></div>
                   <div className={[classes.Dummy,classes.Fandom].join(' ')}></div>
-                 </div>
+                 </div> */}
         </React.Fragment>  
     )
 };
