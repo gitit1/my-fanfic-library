bb289c39fec69fb38561d702d308a56c9c35bc86
Gitit <gitit1@msn.com>
2019-08-25 10:11:09 +0300

    1

--- client/src/store/axios-server.js
+++ client/src/store/axios-server.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 console.log('process.env.NODE_ENV:',process.env.NODE_ENV)
 
 const instance = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') 
-                 ? axios.create({baseURL: 'http://localhost:5555'})
-                 : axios.create({baseURL: 'http://192.236.176.82:5555'});
+                 ? axios.create({baseURL: 'http://localhost:5000'})
+                 : axios.create({baseURL: 'http://192.236.176.82:5000'});
 
 export default instance;
