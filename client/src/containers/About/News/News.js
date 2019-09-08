6b97871059d3acf27c1d91a64dff64b96c638902
Gitit <gitit1@msn.com>
2019-09-08 15:49:43 +0300

    design start for News.js

--- client/src/containers/About/News/News.js
+++ client/src/containers/About/News/News.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Container from '../../../components/UI/Container/Container';
-
+import classes from './News.module.scss'
 const News = () => (
   <Container header='News & Updates'>
-    <p><strong>09-08-2019:</strong>Added "Fandom Universe" attribute to Fandoms on site</p>
+    <div className={classes.News}>
+        <p><span className={classes.Date}><strong>09-07-2019:</strong></span><span className={classes.Content}>"Login" and "Registrer" Pages got Design</span></p>
+        <p><span className={classes.Date}><strong>09-08-2019:</strong></span><span className={classes.Content}>Added "Fandom Universe" attribute to Fandoms on site</span></p>
+    </div>
   </Container>
 );
 
