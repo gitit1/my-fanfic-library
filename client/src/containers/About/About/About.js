57282e14c60723a32fa3357c7e7255db95bd9e38
Gitit <gitit1@msn.com>
2019-09-09 11:22:59 +0300

    [004-git] Added First Information + design to About.js

--- client/src/containers/About/About/About.js
+++ client/src/containers/About/About/About.js
@@ -1,10 +1,77 @@
 import React from 'react';
-import Container from '../../../components/UI/Container/Container';
+import PropTypes from 'prop-types';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-const About = () => (
-  <Container header='About Page'>
-    <img src="/images/work-in-progress.jpg" alt="wip"/>
-  </Container>
-);
+import  './About.scss';
+import Tab0 from './Sections/Tab0'
+import Tab1 from './Sections/Tab1'
+import Tab2 from './Sections/Tab2'
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+
+const About = () => {
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  return(
+    // 
+      <div className='About'>
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          className='Tabs'
+        >
+          <Tab label="How I built the site?" {...a11yProps(0)} />
+          <Tab label="Why built the site?" {...a11yProps(1)} />
+          <Tab label="Funcionality" {...a11yProps(2)} />
+        </Tabs>
+
+        <TabPanel value={value} index={0}><Tab0 /></TabPanel>
+        <TabPanel value={value} index={1}><Tab1 /></TabPanel>
+        <TabPanel value={value} index={2}><Tab2 /></TabPanel>
+      </div>
+    // </Container>
+
+  )
+};
 
 export default About;
---
+++ client/src/containers/About/About/Sections/Tab2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from '../../../../components/UI/Container/Container';
+
+const Tab2 = () => (
+    <Container header='Funcionality'>
+        <section className='Tab2'>
+        </section>
+    </Container>
+);
+
+export default Tab2;
