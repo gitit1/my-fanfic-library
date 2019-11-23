c6d46c2600b545e5d107885853d37f1683b2b049
Gitit <gitit1@msn.com>
2019-11-23 10:45:00 +0200

    trying to fix server routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import * as serviceWorker from './serviceWorker';
@@ -11,9 +11,9 @@ import './index.scss'
 
 const app =(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 )
 
