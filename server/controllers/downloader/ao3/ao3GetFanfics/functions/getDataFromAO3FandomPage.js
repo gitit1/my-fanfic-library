b28044668c42059abc8f17d41017f1db4cbc2a91
Gitit <gitit1@msn.com>
2019-11-23 15:43:26 +0200

    trying to fix downloader

--- server/controllers/downloader/ao3/ao3GetFanfics/functions/getDataFromAO3FandomPage.js
+++ server/controllers/downloader/ao3/ao3GetFanfics/functions/getDataFromAO3FandomPage.js
@@ -11,32 +11,29 @@ exports.getDataFromAO3FandomPage =  async (jar,page,fandom,savedNotAuto) => {
         let n = $('ol.work').children('li').length;
         const limit = pLimit(1)  
         let promises = [];
-        // let counter;
+        let counter;
         for(let count = 0; count < n; count++){
+            console.log('sleeping...');
+            await sleep(8000);
+            console.log('done sleeping...');
             let page = $('ol.work').children('li').eq(count);
-            promises.push(limit(() =>{
-                getDataFromFanficPage(jar,page,fandom.FandomName,fandom.SavedFanficsLastUpdate,fandom.AutoSave,fandom.SaveMethod,savedNotAuto).then(()=>{
-                    wait(7000)
-                });
-            } ));
-            // await getDataFromFanficPage(jar,page,fandom.FandomName,fandom.SavedFanficsLastUpdate,fandom.AutoSave,fandom.SaveMethod,savedNotAuto).then(res=>{
-            //     donePromise++;
-            //     res===0 && counter++;
+            // await promises.push(limit(() =>{
+            //     getDataFromFanficPage(jar,page,fandom.FandomName,fandom.SavedFanficsLastUpdate,fandom.AutoSave,fandom.SaveMethod,savedNotAuto)
+            // } ));
 
-            // }).then(
-            //     new Promise(resolve => setTimeout(resolve, 5000))
-            // )
-            // if (donePromise == n) {return counter}                
+            await getDataFromFanficPage(jar,page,fandom.FandomName,fandom.SavedFanficsLastUpdate,fandom.AutoSave,fandom.SaveMethod,savedNotAuto).then(res=>{
+                donePromise++;
+                res===0 && counter++;   
+            }).then(()=>{new Promise(resolve => setTimeout(() => resolve(), 7000))})
+            if (donePromise == n) {return counter}                
         }
-        await Promise.all(promises).then(async results=> {
-            return n;
-        });
+        // await Promise.all(promises).then(async results=> {
+        //     return n;
+        // });
         
     } catch(e) {console.log(e);}
 }
 
-wait = (ms) => {
-    return function(v) {
-      return new Promise(resolve => setTimeout(() => resolve(v), ms));
-    };
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
