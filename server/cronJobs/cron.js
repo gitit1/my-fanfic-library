674470dd83a774835a309759a14002fe05be2162
Gitit <gitit1@msn.com>
2020-01-03 18:01:19 +0200

    [036-git] - little fix for deleted fanfics fownload + cron

--- server/cronJobs/cron.js
+++ server/cronJobs/cron.js
@@ -3,16 +3,17 @@ const clc = require("cli-color");
 
 const getFanfics = require('../controllers/connection/connection');
  
+console.log('in cron file')
 //Frequency: At 02:00 on every day-of-week from Sunday through Thusday. (https://crontab.guru/)
 cron.schedule('0 2 * * sun-thu', () => {
     console.log(clc.bgRed('CronJob Awoke: [Get Fanfics of all fandoms - Partial Run]'));
-    getFanfics.manageDownloader(null,'All','getFandomFanficsPartial','partial','cron')
+    getFanfics.manageDownloader(null,'All','getFandomFanficsPartial','cron')
 });
 
 //At 1:00 on Friday.
 cron.schedule('0 1 * * fri', async ()=>{
     console.log(clc.bgRed('CronJob Awoke: [Get Fanfics of all fandoms - Partial  Run]'));
-    await getFanfics.manageDownloader(null,'All','getFandomFanficsPartial','partial');
+    await getFanfics.manageDownloader(null,'All','getFandomFanficsPartial','cron');
     console.log(clc.bgRed('CronJob Awoke: [Get Deleted Fanfics of all fandoms]'));
     await getFanfics.manageDownloader(null,'All','getDeletedFanfics','cron');   
 })
@@ -20,7 +21,7 @@ cron.schedule('0 1 * * fri', async ()=>{
 //At 1:00 on Saturday.
 cron.schedule('0 23 * * sat', async ()=>{
     console.log(clc.bgRed('CronJob Awoke: [Get Fanfics of all fandoms - Full run]'));
-    await getFanfics.manageDownloader(null,'All','getFandomFanficsPartial','full','cron');   
+    await getFanfics.manageDownloader(null,'All','getFandomFanficsPartial','cron');   
 })
 
 //TODO: ARCHIVE LATESTUPDATE - IF COUNT > LIMIT - TAKE THE EXTRA TO ARCHIVE BY MONTH;
