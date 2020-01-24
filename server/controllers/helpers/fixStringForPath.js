2f6664b30349abc783ee05adf68c01e66f9f3422
Gitit <gitit1@msn.com>
2020-01-24 11:54:58 +0200

    added a hypersentence to fix string

--- server/controllers/helpers/fixStringForPath.js
+++ server/controllers/helpers/fixStringForPath.js
@@ -6,6 +6,7 @@ exports.fixStringForPath = (fileName) =>{
     newFileName = newFileName.replace(/\?|\!/g, "");
     newFileName = newFileName.replace(/\"/g, "");
     newFileName = newFileName.replace(/é/g, "e");
+    newFileName = newFileName.replace(/%22/g, "");
     console.log('[db controller] fixStringForPath: newFileName',newFileName);
     return newFileName;
 }
