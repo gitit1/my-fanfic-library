3d97e29f6621337637e993c8a7b15d7834be73f8
Gitit <gitit1@msn.com>
2020-03-27 16:45:07 +0300

    fix alignment of image in fanfic page + added new category: 'NSFW'

--- client/src/containers/Fanfic/components/ShowFanficData/FanficData/Categories/assets/categoriesList.js
+++ client/src/containers/Fanfic/components/ShowFanficData/FanficData/Categories/assets/categoriesList.js
@@ -57,6 +57,7 @@ export const categories = [
   { label: 'Music' },
   { label: 'Musician' },
   { label: 'Neighbors' },
+  { label: 'NSFW' },
   { label: 'Pirates' },
   { label: 'Police' },
   { label: 'Political' },
