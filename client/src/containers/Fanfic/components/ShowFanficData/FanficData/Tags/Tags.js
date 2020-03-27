cde0722ba4c2ece5a8077277229db1df48a644dc
Gitit <gitit1@msn.com>
2020-03-27 20:53:53 +0300

    fixed the bug

--- client/src/containers/Fanfic/components/ShowFanficData/FanficData/Tags/Tags.js
+++ client/src/containers/Fanfic/components/ShowFanficData/FanficData/Tags/Tags.js
@@ -5,18 +5,20 @@ import Typography from '@material-ui/core/Typography';
 const Tags = (props) => (
     <React.Fragment>
         {
-            props.fanfic.Tags.map((tags)=>(
-                <div key={Object.keys(tags)[0]}>
-                    {Object.keys(tags)[0].length>0 && <Typography variant="subtitle2" gutterBottom  className='card_content_tags_caption'>{ `${Object.keys(tags)[0]}: `}</Typography>}
-                    <div className='card_content_tags_div'>
-                        {tags[Object.keys(tags)[0]].map(tag=>(                                           
-                            // <span key={tag} >{tag}</span>
-                            <Chip size="small" key={tag} label={tag} className='tags_chip' onClick={()=>props.filter(tag,null,'tag',Object.keys(tags)[0])}/>
-                        ))}
-                    </div>                                                  
-                </div>
-    
-            ))
+            props.fanfic.Tags.map((tags)=>{
+                return(
+                    tags[Object.keys(tags)[0]].length>0 && 
+                    <div key={Object.keys(tags)[0]}>
+                        <Typography variant="subtitle2" gutterBottom  className='card_content_tags_caption'>{ `${Object.keys(tags)[0]}: `}</Typography>
+                        <div className='card_content_tags_div'>
+                            {tags[Object.keys(tags)[0]].map(tag=>(                                           
+                                <Chip size="small" key={tag} label={tag} className='tags_chip' onClick={()=>props.filter(tag,null,'tag',Object.keys(tags)[0])}/>
+                            ))}
+                        </div>                                                  
+                    </div>
+                    
+                )
+            })
         }
     </React.Fragment>
 );
