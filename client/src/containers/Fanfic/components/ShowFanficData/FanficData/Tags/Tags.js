3bea9e1ed2083ba6a2c305ee683e3f82a8d39f2c
Gitit <gitit1@msn.com>
2020-03-27 20:54:28 +0300

    foramated the component

--- client/src/containers/Fanfic/components/ShowFanficData/FanficData/Tags/Tags.js
+++ client/src/containers/Fanfic/components/ShowFanficData/FanficData/Tags/Tags.js
@@ -5,18 +5,18 @@ import Typography from '@material-ui/core/Typography';
 const Tags = (props) => (
     <React.Fragment>
         {
-            props.fanfic.Tags.map((tags)=>{
-                return(
-                    tags[Object.keys(tags)[0]].length>0 && 
+            props.fanfic.Tags.map((tags) => {
+                return (
+                    tags[Object.keys(tags)[0]].length > 0 &&
                     <div key={Object.keys(tags)[0]}>
-                        <Typography variant="subtitle2" gutterBottom  className='card_content_tags_caption'>{ `${Object.keys(tags)[0]}: `}</Typography>
+                        <Typography variant="subtitle2" gutterBottom className='card_content_tags_caption'>{`${Object.keys(tags)[0]}: `}</Typography>
                         <div className='card_content_tags_div'>
-                            {tags[Object.keys(tags)[0]].map(tag=>(                                           
-                                <Chip size="small" key={tag} label={tag} className='tags_chip' onClick={()=>props.filter(tag,null,'tag',Object.keys(tags)[0])}/>
+                            {tags[Object.keys(tags)[0]].map(tag => (
+                                <Chip size="small" key={tag} label={tag} className='tags_chip' onClick={() => props.filter(tag, null, 'tag', Object.keys(tags)[0])} />
                             ))}
-                        </div>                                                  
+                        </div>
                     </div>
-                    
+
                 )
             })
         }
