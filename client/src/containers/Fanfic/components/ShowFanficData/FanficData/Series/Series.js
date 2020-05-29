58603ba0d68ca3a09dc72e97c3fee42bb7b59882
Gitit <gitit1@msn.com>
2020-05-29 11:01:30 +0300

    fix general bug with href of series

--- client/src/containers/Fanfic/components/ShowFanficData/FanficData/Series/Series.js
+++ client/src/containers/Fanfic/components/ShowFanficData/FanficData/Series/Series.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Series = (props) => {
     const {Series,SeriesPart,SeriesURL} = props.fanfic;
+    const hasSeries = (!Series || Series==='false' || Series===undefined || Series===null) ? false : true;
     return(
         <React.Fragment>
-            {(Series!=='false'&&Series!==undefined&&Series!==null) &&  <p>Part <span>{SeriesPart}</span> of <a href={SeriesURL} target='_blank' rel="noopener noreferrer">{Series}</a></p> }
+            { hasSeries && 
+                <p>Part <span>{SeriesPart}</span> of <a href={SeriesURL} target='_blank' rel="noopener noreferrer">{Series}</a></p> 
+            }
         </React.Fragment>
     )
 };
