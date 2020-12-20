5d0f4a22c8e0b5be8475c95c9bce565af4dce671
Gitit <gitit1@msn.com>
2020-12-20 22:33:33 +0200

    changed getUrlBodyFromSite funciton to use puppeteer instead of request - to avoid cloudflare capcha

--- server/controllers/downloader/ff/helpers/getUrlBodyFromSite.js
+++ server/controllers/downloader/ff/helpers/getUrlBodyFromSite.js
@@ -1,18 +1,23 @@
-const clc = require("cli-color");
+const clc = require ('cli-color');
+const puppeteer = require('puppeteer');
 
-let request = require('request')
-let jar = request.jar();
-request = request.defaults({jar: jar,followAllRedirects: true});
-
-exports.getUrlBodyFromSite = url =>{
-    return new Promise(function(resolve, reject) {
-        request.get({url,jar,gzip: true,credentials: 'include'}, function (err, httpResponse, body) {
-            if(err){  
-                console.log(clc.red('Error in getUrlBodyFromSite()',err))                  
-                reject(false)
-            }else{
-                resolve(httpResponse.body)
-            }        
-        });
+exports.getUrlBodyFromSite = url => {
+  console.log(clc.blue(`[ff controller] getUrlBodyFromSite()`));
+  return new Promise (async function (resolve, reject) {
+    const browser = await puppeteer.launch(
+        {
+        ignoreHTTPSErrors: true,
+        args :[
+          '--ignore-certificate-errors',
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--lang=ja,en-US;q=0.9,en;q=0.8',
+          '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+        ]
     });
-}
+    const page = await browser.newPage();
+    const response = await page.goto(url);
+    resolve (await response.text());
+  });
+};
