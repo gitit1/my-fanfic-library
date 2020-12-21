1c9287c5be0e6582aa097e6251e2ee0aa0169ce4
Gitit <gitit1@msn.com>
2020-12-21 12:03:54 +0200

    fixed pupeteer crash by closing the page and browser

--- server/controllers/downloader/ff/helpers/getUrlBodyFromSite.js
+++ server/controllers/downloader/ff/helpers/getUrlBodyFromSite.js
@@ -1,25 +1,31 @@
 const clc = require ('cli-color');
-const puppeteer = require('puppeteer');
+const puppeteer = require ('puppeteer');
 
 exports.getUrlBodyFromSite = url => {
-  console.log(clc.blue(`[ff controller] getUrlBodyFromSite()`));
+  console.log (clc.blue (`[ff controller] getUrlBodyFromSite()`));
   return new Promise (async function (resolve, reject) {
-    const browser = await puppeteer.launch(
-        {
-        ignoreHTTPSErrors: true,
-        args :[
-          '--ignore-certificate-errors',
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage',
-          '--lang=ja,en-US;q=0.9,en;q=0.8',
-          '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
-        ]
+    const browser = await puppeteer.launch({
+      ignoreHTTPSErrors: true,
+      args: [
+        '--ignore-certificate-errors',
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--lang=ja,en-US;q=0.9,en;q=0.8',
+        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+      ],
     });
     const page = await browser.newPage();
-    const response = await page.goto(url);
-    await page.close();
-    await browser.close();
-    resolve (await response.text());
+    try {
+      const response = await page.goto(url);
+      resolve (await response.text());
+    } catch (error) {
+      console.log ('error in getUrlBodyFromSite:', error);
+      reject ();
+    } finally {
+      console.log ('getUrlBodyFromSite::: Finally close connection');
+      await page.close();
+      await browser.close();
+    }
   });
 };
