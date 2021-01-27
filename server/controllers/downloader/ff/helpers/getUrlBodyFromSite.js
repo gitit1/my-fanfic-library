4ab3773fd1c4a674bae345d05c98823683690798
gitit1 <gitit1@msn.com>
2021-01-27 15:52:41 +0200

    fix ff issue with timeout for puppeteer

--- server/controllers/downloader/ff/helpers/getUrlBodyFromSite.js
+++ server/controllers/downloader/ff/helpers/getUrlBodyFromSite.js
@@ -2,7 +2,7 @@ const clc = require ('cli-color');
 const puppeteer = require ('puppeteer');
 
 exports.getUrlBodyFromSite = url => {
-  console.log (clc.blue (`[ff controller] getUrlBodyFromSite()`));
+  console.log (clc.blue (`[ff controller] getUrlBodyFromSite(): `, url));
   return new Promise (async function (resolve, reject) {
     const browser = await puppeteer.launch({
       ignoreHTTPSErrors: true,
@@ -13,9 +13,13 @@ exports.getUrlBodyFromSite = url => {
         '--disable-dev-shm-usage',
         '--lang=ja,en-US;q=0.9,en;q=0.8',
         '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+		    '--headless'
       ],
     });
+    
     const page = await browser.newPage();
+	  page.setDefaultNavigationTimeout(0);
+	
     try {
       const response = await page.goto(url);
       resolve (await response.text());
